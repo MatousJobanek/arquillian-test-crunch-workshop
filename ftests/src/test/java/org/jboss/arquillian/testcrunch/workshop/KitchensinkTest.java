8322b5cd9c0bbc9065c7692a51752ac85a840b2d
Matous Jobanek <mjobanek@redhat.com>
2015-11-03 08:25:29 +0100

    cleanup

--- ftests/src/test/java/org/jboss/arquillian/testcrunch/workshop/KitchensinkTest.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.arquillian.testcrunch.workshop;
-
-/**
- * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
- */
-public class KitchensinkTest {
-
-}
---
+++ ui-ftests/src/test/java/org/jboss/arquillian/kitchensink/ui/ftests/KitchensinkTest.java
@@ -0,0 +1,14 @@
+package org.jboss.arquillian.kitchensink.ui.ftests;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
+ */
+public class KitchensinkTest {
+
+    @Test
+    public void simpleTest(){
+        System.out.println("Hi there, I'm running!!!");
+    }
+}
