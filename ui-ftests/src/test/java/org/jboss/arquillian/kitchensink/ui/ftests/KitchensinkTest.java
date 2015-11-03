ababd8c5a447d57630341ba683f483d492435cc9
Matous Jobanek <mjobanek@redhat.com>
2015-11-03 22:58:06 +0100

    added container

--- ui-ftests/src/test/java/org/jboss/arquillian/kitchensink/ui/ftests/KitchensinkTest.java
+++ ui-ftests/src/test/java/org/jboss/arquillian/kitchensink/ui/ftests/KitchensinkTest.java
@@ -1,6 +1,12 @@
 package org.jboss.arquillian.kitchensink.ui.ftests;
 
+import java.io.File;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -10,8 +16,19 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class KitchensinkTest {
 
+    @Deployment
+    public static WebArchive deploy() {
+        return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../kitchensink/target/jboss-kitchensink.war"));
+    }
+
     @Test
-    public void simpleTest(){
-        System.out.println("Hi there, I'm running!!!");
+    @RunAsClient
+    public void clientTest() {
+        System.out.println("Hi there, I'm running on client!!!");
+    }
+
+    @Test
+    public void serverTest() {
+        System.err.println("Hi there, I'm running in container!!!");
     }
 }
