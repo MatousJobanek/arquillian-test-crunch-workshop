b70e81dbcd0b60fc20478eb514055d3e8be9447a
Matous Jobanek <mjobanek@redhat.com>
2015-11-05 11:04:47 +0100

    added combination of both approaches

--- ui-ftests/src/test/java/org/jboss/arquillian/kitchensink/ui/ftests/KitchensinkTest.java
+++ ui-ftests/src/test/java/org/jboss/arquillian/kitchensink/ui/ftests/KitchensinkTest.java
@@ -4,15 +4,21 @@ import java.io.File;
 import java.net.URL;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.findby.FindByJQuery;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.quickstarts.kitchensink.data.MemberRepository;
+import org.jboss.as.quickstarts.kitchensink.model.Member;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,12 +31,19 @@ import org.openqa.selenium.support.FindBy;
  * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
  */
 @RunWith(Arquillian.class)
-@RunAsClient
 public class KitchensinkTest {
 
     @Deployment
     public static WebArchive deploy() {
-        return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../kitchensink/target/jboss-kitchensink.war"));
+        File[] droneDependencies = Maven
+            .configureResolver()
+            .loadPomFromFile("pom.xml")
+            .resolve("org.jboss.arquillian.extension:arquillian-drone-webdriver-depchain:pom:2.0.0.Alpha5")
+            .withTransitivity()
+            .asFile();
+
+        return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../kitchensink/target/jboss-kitchensink.war"))
+            .addAsLibraries(droneDependencies);
     }
 
     @ArquillianResource
@@ -54,7 +67,12 @@ public class KitchensinkTest {
     @FindByJQuery("table.simpletablestyle > tbody > tr")
     private List<WebElement> tableRows;
 
+    @Inject
+    private MemberRepository memberRepository;
+
     @Test
+    @InSequence(1)
+    @RunAsClient
     public void registerMemberTest() {
         browser.get(url.toString());
 
@@ -73,6 +91,17 @@ public class KitchensinkTest {
         Assert.assertEquals("My cool name", columns.get(1).getText());
         Assert.assertEquals("my@cool.email", columns.get(2).getText());
         Assert.assertEquals("1234567890", columns.get(3).getText());
+    }
+
+    @Test
+    @InSequence(2)
+    public void verifyRegistrationTest() {
+        Assert.assertNotNull(memberRepository);
+
+        Member found = memberRepository.findByEmail("my@cool.email");
+        Assert.assertNotNull(found);
+        Assert.assertEquals("My cool name", found.getName());
+        Assert.assertEquals("1234567890", found.getPhoneNumber());
     }
 
 }
